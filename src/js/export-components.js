c7f30abb385ff5be77a085728a0d821da4881050
akainic <Anne.Kainic@va.gov>
2017-11-03 16:01:27 -0400

    Add code to transform components using babel

---
+++ src/js/export-components.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const babel = require("babel-core");
+
+const reactComponentFiles = [
+  'src/components/modal/Modal.js'
+];
+
+reactComponentFiles.forEach((fileName) => {
+  babel.transformFile(fileName, (err, result) => {
+    const componentName = fileName.slice(fileName.lastIndexOf('/')+1, fileName.indexOf('.'));
+    const newFileName = `dist/js/components/${componentName}.js`;
+
+    fs.writeFile(newFileName, result.code);
+  });
+});
