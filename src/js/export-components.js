e71bafd0f6056a86c24a69107df294010c26b6d6
akainic <Anne.Kainic@va.gov>
2017-11-03 16:12:09 -0400

    Make it more readable

--- src/js/export-components.js
+++ src/js/export-components.js
@@ -7,7 +7,7 @@ const reactComponentFiles = [
 
 reactComponentFiles.forEach((fileName) => {
   babel.transformFile(fileName, (err, result) => {
-    const componentName = fileName.slice(fileName.lastIndexOf('/')+1, fileName.indexOf('.'));
+    const componentName = fileName.slice(fileName.lastIndexOf('/') + 1, fileName.indexOf('.'));
     const newFileName = `dist/js/components/${componentName}.js`;
 
     fs.writeFile(newFileName, result.code);
