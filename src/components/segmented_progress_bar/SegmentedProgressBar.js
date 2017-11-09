884dac7cf0dd2f51e1b7d02aeb16103e075f33a3
akainic <Anne.Kainic@va.gov>
2017-11-09 12:28:11 -0500

    Render component correctly, still need to figure out styles

--- src/components/segmented_progress_bar/SegmentedProgressBar.js
+++ src/components/segmented_progress_bar/SegmentedProgressBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import _ from 'lodash';
 
 /**
@@ -16,3 +17,8 @@ export default function SegmentedProgressBar({ current, total }) {
     </div>
   );
 }
+
+SegmentedProgressBar.propTypes = {
+  current: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired
+};
