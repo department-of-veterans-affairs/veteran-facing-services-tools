30857ae641fb79ff1a46f347ab0f083617fed56b
Anne Kainic <annekainicUSDS@users.noreply.github.com>
2017-11-28 12:00:18 -0500

    Add doc on moving components (#12)
    * Add steps to moving a component into the design system
    
    Add code quotes
    
    Amend a couple of things
    
    * Add comments from review
    
    * Fix link encoding

--- dist/styles.bundle.js
+++ dist/styles.bundle.js
@@ -71,6 +71,7 @@
 /***/ 87:
 /***/ (function(module, exports) {
 
+throw new Error("Module build failed: ModuleBuildError: Module build failed: \n  @include focus;\n          ^\n      No mixin named focus\n\nBacktrace:\n\tnode_modules/uswds/src/stylesheets/core/_base.scss:34\n      in /Users/vacokainia/Desktop/coding/design-system/node_modules/uswds/src/stylesheets/core/_base.scss (line 34, column 12)\n    at runLoaders (/Users/vacokainia/Desktop/coding/design-system/node_modules/webpack/lib/NormalModule.js:192:19)\n    at /Users/vacokainia/Desktop/coding/design-system/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /Users/vacokainia/Desktop/coding/design-system/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/Users/vacokainia/Desktop/coding/design-system/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/Users/vacokainia/Desktop/coding/design-system/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.<anonymous> (/Users/vacokainia/Desktop/coding/design-system/node_modules/async/dist/async.js:2257:31)\n    at Object.callback (/Users/vacokainia/Desktop/coding/design-system/node_modules/async/dist/async.js:958:16)\n    at options.error (/Users/vacokainia/Desktop/coding/design-system/node_modules/node-sass/lib/index.js:294:32)");
 
 /***/ })
 
