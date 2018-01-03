ae79e98f06554c57185e8f5ce1c8a5da8bc5e7d7
Jeff Balboni <jeff@adhocteam.us>
2018-01-03 17:43:27 -0500

    Updates

--- dist/styles.bundle.js
+++ dist/styles.bundle.js
@@ -63,12 +63,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 87);
+/******/ 	return __webpack_require__(__webpack_require__.s = 88);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 87:
+/***/ 88:
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
