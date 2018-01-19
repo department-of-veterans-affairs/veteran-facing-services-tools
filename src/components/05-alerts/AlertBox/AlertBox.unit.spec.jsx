c1ce117c4e4b4f264f34aa79123648a015f79f1b
Sophia Dengo <sdengo@gmail.com>
2018-01-19 11:15:23 -0500

    added more axechecks

--- src/components/05-alerts/AlertBox/AlertBox.unit.spec.jsx
+++ src/components/05-alerts/AlertBox/AlertBox.unit.spec.jsx
@@ -7,6 +7,10 @@ import AlertBox from './AlertBox.jsx';
 
 // Placeholder for required "content" element
 const Content = (<p/>);
+const Headline = 'Headline';
+function closeAlert() {
+  //
+}
 
 describe('<AlertBox />', () => {
   it('should be an empty div if invisible', () => {
@@ -28,9 +32,40 @@ describe('<AlertBox />', () => {
     expect(wrapper.find('.usa-alert').hasClass('usa-alert-info')).to.equal(true);
   });
 
-  it('should pass aXe check', () => {
+  it('should pass aXe check when visible', () => {
     return axeCheck(<AlertBox
       content={Content}
+      status={'info'}
+      isVisible/>);
+  });
+
+  it('should pass aXe check when not visible', () => {
+    return axeCheck(<AlertBox
+      content={Content}
+      status={'info'}
+      isVisible={false}/>);
+  });
+
+  it('should pass aXe check without a headline', () => {
+    return axeCheck(<AlertBox
+      content={Content}
+      status={'info'}
+      isVisible/>);
+  });
+
+  it('should pass aXe check with a headline', () => {
+    return axeCheck(<AlertBox
+      headline={Headline}
+      content={Content}
+      status={'info'}
+      isVisible/>);
+  });
+
+  it('should pass aXe check when it has a close button', () => {
+    return axeCheck(<AlertBox
+      headline={Headline}
+      content={Content}
+      onCloseAlert={closeAlert()}
       status={'info'}
       isVisible/>);
   });
