5b0b8a32337178ac97c261d9ab8cb7964a67cd1a
Sophia Dengo <sdengo@gmail.com>
2018-01-25 15:35:37 -0500

    relinked files broken by org move

--- src/components/07-form/controls/errorable_checkbox/ErrorableCheckbox.jsx
+++ src/components/07-form/controls/errorable_checkbox/ErrorableCheckbox.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import _ from 'lodash';
-import ToolTip from '../tooltip/Tooltip.jsx'; // File extension provided for test
+import ToolTip from '../../../tooltip/Tooltip.jsx'; // File extension provided for test
 
 /**
  * A form checkbox with a label that can display error messages.
