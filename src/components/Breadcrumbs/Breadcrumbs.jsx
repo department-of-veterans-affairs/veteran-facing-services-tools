5d19ed23bf2168ebe58f8a822a4584d4f074f8d1
Sophia Dengo <sdengo@gmail.com>
2018-02-08 11:38:52 -0500

    modified breadcrumbs to be generic and take crumbs as props

--- src/components/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,33 +1,17 @@
-import { Link, browserHistory } from 'react-router';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 class Breadcrumbs extends React.Component {
   render() {
-    const { pathname, query: { version } } = this.props.location;
 
-    const crumbs = [
-      <a href="/" key="home">Home</a>,
-      <a href="/education/" key="education">Education Benefits</a>,
-      <a href="/education/gi-bill/" key="gi-bill">GI Bill</a>,
-    ];
-
-    if (pathname.match(/search|profile/)) {
-      const root = { pathname: '/', query: (version ? { version } : {}) };
-      crumbs.push(<Link to={root} key="main">GI Bill® Comparison Tool</Link>);
-    }
-
-    if (pathname.match(/profile/)) {
-      if (this.props.includeSearch) {
-        crumbs.push(<a onClick={browserHistory.goBack} key="search-results">Search Results</a>);
-      }
-    }
+    const crumbs = this.props.crumbs;
+    console.log(typeof crumbs);
 
     return (
       <nav className="va-nav-breadcrumbs">
         <ul className="row va-nav-breadcrumbs-list columns" role="menubar" aria-label="Primary">
-          {crumbs.map((c, i) => {
-            return <li key={i}>{c}</li>;
+          {crumbs.map((c) => {
+            return <li key={c.key}><a href={c.link}>{c.label}</a></li>;
           })}
         </ul>
       </nav>
@@ -36,7 +20,10 @@ class Breadcrumbs extends React.Component {
 }
 
 Breadcrumbs.propTypes = {
-  includeSearch: PropTypes.bool,
+
+  // array should contain objects that contain each breadcrumb's
+  // key, href, and plain-text label
+  crumbs: PropTypes.arrayOf(PropTypes.object).isRequired
 };
 
 export default Breadcrumbs;
