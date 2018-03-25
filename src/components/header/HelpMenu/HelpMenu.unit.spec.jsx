3a0c68eef42eeaaa73d1fa7b23a026b4c8c97d35
Sophia Dengo <sdengo@gmail.com>
2018-03-25 08:35:54 -0400

    added axecheck; tests pass

--- src/components/header/HelpMenu/HelpMenu.unit.spec.jsx
+++ src/components/header/HelpMenu/HelpMenu.unit.spec.jsx
@@ -1,21 +1,33 @@
-
 import React from 'react';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
-// import { axeCheck } from '../../../../lib/testing/helpers';
+import { axeCheck } from '../../../../lib/testing/helpers';
 
 import HelpMenu from './HelpMenu.jsx';
 
 describe('<HelpMenu>', () => {
-  const props = {
-    isOpen: false,
-    clickHandler: f => f
-  };
-
-  const tree = shallow(<HelpMenu {...props}/>);
 
   it('should render', () => {
-    const vdom = tree.getRenderOutput();
-    expect(vdom).to.not.be.undefined;
+    const props = {
+      isOpen: false,
+      clickHandler: f => f
+    };
+
+    const tree = shallow(<HelpMenu {...props}/>);
+    expect(tree).to.have.length(1);
+  });
+
+  it('should pass axeCheck when open', () => {
+    const props = {
+      clickHandler: f => f
+    };
+    return axeCheck(<HelpMenu isOpen {...props}/>);
+  });
+
+  it('should pass axeCheck when closed', () => {
+    const props = {
+      clickHandler: f => f
+    };
+    return axeCheck(<HelpMenu isOpen={false} {...props}/>);
   });
 });
