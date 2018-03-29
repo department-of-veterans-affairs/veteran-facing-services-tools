3da8cfb5e21c7499b807b2b2ae9b77a17e631cdd
Sophia Dengo <sdengo@gmail.com>
2018-03-29 14:20:38 -0400

    light themeing

--- fractal.js
+++ fractal.js
@@ -14,7 +14,7 @@ const context = {
   isProduction: process.env.NODE_ENV === 'production'
 };
 
-fractal.set('project.title', 'Jean Pants');
+fractal.set('project.title', '');
 
 const { components, docs, web } = fractal;
 
