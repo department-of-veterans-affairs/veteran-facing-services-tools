09093d39ab5a33efcd9289be9e319bd07ac3b910
Trevor Pierce <1Copenut@users.noreply.github.com>
2018-04-17 15:42:33 -0500

    [10019-breadcrumb-styles] Moving from the Vets.gov website into design-system (#103)
    * First commit, adding breadcrumb styles and markup from Vets.gov website.
    
    * Adding comments to PropTypes checking for ID.
    
    * Adding in version bump.

--- src/components/navigation/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/navigation/Breadcrumbs/Breadcrumbs.jsx
@@ -3,24 +3,38 @@ import React from 'react';
 
 class Breadcrumbs extends React.Component {
   render() {
-    const crumbs = this.props.crumbs;
+    const { crumbs, id } = this.props;
     return (
-      <nav className="va-nav-breadcrumbs">
-        <ul className="row va-nav-breadcrumbs-list columns" role="menubar" aria-label="Primary">
-          {crumbs.map((c) => {
-            return <li key={c.key}><a href={c.link}>{c.label}</a></li>;
+      <nav
+        aria-label="Breadcrumb"
+        className="va-nav-breadcrumbs"
+        id={id}>
+        <ol className="row va-nav-breadcrumbs-list columns" id={`${id}-list`}>
+          {crumbs.map(c => {
+            return (
+              <li key={c.key}>
+                <a
+                  aria-current={c.ariaCurrent ? 'page' : null}
+                  href={c.link}>
+                  {c.label}
+                </a>
+              </li>
+            );
           })}
-        </ul>
+        </ol>
       </nav>
     );
   }
 }
 
 Breadcrumbs.propTypes = {
-
   // array should contain objects that contain each breadcrumb's
   // key, href, and plain-text label
-  crumbs: PropTypes.arrayOf(PropTypes.object).isRequired
+  crumbs: PropTypes.arrayOf(PropTypes.object).isRequired,
+  // Passing a prop `id='STRING'` into the `<Breadcrumb>` component
+  // will append that ID to the `<nav>` element, and concatenate the
+  // ID plus `-list` to the `<ol>` element.
+  id: PropTypes.string
 };
 
 export default Breadcrumbs;
