30ed24e706c60b1ec5d94c17d20027271e34fd2a
Bill Tran <bill@billjohntran.com>
2018-07-24 13:42:36 -0700

    revert back to working version of MegaMenu

--- src/components/navigation/Megamenu/Megamenu.unit.spec.jsx
+++ src/components/navigation/Megamenu/Megamenu.unit.spec.jsx
@@ -1,153 +1,227 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import { expect } from 'chai';
-import Pagination from '../Pagination/Pagination';
+import sinon from 'sinon';
 import { axeCheck } from '../../../../lib/testing/helpers';
+import { mount } from 'enzyme';
 
-const props = {
-  onPageSelect: () => {},
-};
+import MegaMenu from './MegaMenu.jsx';
 
-describe('<Pagination>', () => {
-  it('should show all pages if there are fewer pages than the max', () => {
-    const tree = shallow(
-      <Pagination
-        {...props}
-        page={1}
-        pages={5}/>
-    );
-    const links = tree.find('a');
-    expect(links).to.have.length(6);
-    links.forEach((link, index) => {
-      if (index === 5) {
-        expect(link.text()).to.equal('Next');
-      } else {
-        const pageNumber = (index + 1).toString();
-        expect(link.text()).to.equal(pageNumber);
+const data = [
+  {
+    title: 'Nav Title 1',
+    menuSections: [
+      {
+        title: 'Section Title 1',
+        links: {
+          columnOne: {
+            title: 'Menu Item 1',
+            links: [
+              {
+                text: 'First link',
+                href: '#'
+              }
+            ]
+          },
+          columnTwo: {
+            title: 'Column 2 title',
+            links: [
+              {
+                text: 'First link',
+                href: '#'
+              }
+            ]
+          },
+          columnThree: {
+            img: {
+              src: 'http://via.placeholder.com/228x128',
+              alt: 'Place Holder Image'
+            },
+            link: {
+              text: 'Text for link',
+              href: '#'
+            },
+            description: 'Add a description of the marketing content'
+          },
+          seeAllLink: {
+            text: 'Link to menu page',
+            href: '#'
+          }
+        }
+      },
+      {
+        title: 'Section Title 2',
+        links: {
+          columnOne: {
+            title: 'Menu Item 1',
+            links: [
+              {
+                text: 'First link',
+                href: '#'
+              }
+            ]
+          },
+          columnTwo: {
+            title: 'Column 2 title',
+            links: [
+              {
+                text: 'First link',
+                href: '#'
+              }
+            ]
+          },
+          columnThree: {
+            img: {
+              src: 'http://via.placeholder.com/228x128',
+              alt: 'Place Holder Image'
+            },
+            link: {
+              text: 'Text for Marketing Link',
+              href: '#'
+            },
+            description: 'Add a description of the marketing content'
+          },
+          seeAllLink: {
+            text: 'Link to menu page',
+            href: '#'
+          }
+        }
       }
+    ]
+  },
+  {
+    title: 'Nav Title 2',
+    menuSections: {
+      mainColumn: {
+        title: 'Main Dropdown Section Title',
+        links: [
+          {
+            text: 'First Link',
+            href: '#'
+          },
+        ]
+      },
+      columnOne: {
+        title: 'Column 1 Title',
+        links: [
+          {
+            text: 'Link 1',
+            href: '#'
+          }
+        ]
+      },
+      columnTwo: {
+        title: 'Column 2 Title',
+        links: [
+          {
+            text: 'Link 1',
+            href: '#'
+          },
+        ]
+      },
+      columnThree: {
+        img: {
+          src: 'http://via.placeholder.com/228x128',
+          alt: 'Place Holder Image'
+        },
+        link: {
+          text: 'Text for Marketing Link',
+          href: '#'
+        },
+        description: 'Add a description of the marketing content'
+      },
+    }
+  },
+  {
+    title: 'Nav Title 3 link only',
+    href: '#'
+  }
+];
+
+let div;
+let megaMenu;
+
+describe('<MegaMenu>', () => {
+
+  beforeEach(() => {
+    div = document.createElement('div');
+    div.setAttribute('id', 'mega-menu');
+    document.body.appendChild(div);
+
+    megaMenu = mount(
+      <MegaMenu
+        data={data}
+        toggleDropDown={(title) => { title; }}
+        updateCurrentSection={(title) => { title; }}></MegaMenu>,
+      {
+        attachTo: div
+      }
+    );
+  });
+
+  afterEach(() => {
+    document.body.removeChild(div);
+  });
+
+  it('should render', () => {
+    const menuTexts = ['Home', 'Nav Title 1', 'Nav Title 2', 'Nav Title 3 link only'];
+
+    expect(megaMenu.find('.login-container').exists()).to.be.true;
+
+    megaMenu.find('.vetnav-level1').forEach((el) => {
+      expect(menuTexts.includes(el.text())).to.be.true;
     });
   });
 
-  it('should show both "Prev" and "Next" if in a middle page', () => {
-    const tree = shallow(
-      <Pagination
-        {...props}
-        page={3}
-        pages={5}/>
-    );
-    const links = tree.find('a');
-    expect(links).to.have.length(7);
-    links.forEach((link, index) => {
-      if (index === 0) {
-        expect(link.text()).to.equal('Prev');
-      } else if (index === 6) {
-        expect(link.text()).to.equal('Next');
-      } else {
-        const pageNumber = index.toString();
-        expect(link.text()).to.equal(pageNumber);
-      }
-    });
+  it('should run toggleDropDown if clicked on', () => {
+    const spy = sinon.spy(megaMenu.instance(), 'toggleDropDown');
+    megaMenu.find('button').first().simulate('click');
+
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.getCall(0).args[0]).to.equal('Nav Title 1');
   });
 
-  it('should show "Prev" but not "Next" if on the last page', () => {
-    const tree = shallow(
-      <Pagination
-        {...props}
-        page={5}
-        pages={5}/>
-    );
-    const links = tree.find('a');
-    expect(links).to.have.length(6);
-    links.forEach((link, index) => {
-      if (index === 0) {
-        expect(link.text()).to.equal('Prev');
-      } else if (index === 6) {
-        expect(link.text()).to.equal('Next');
-      } else {
-        const pageNumber = index.toString();
-        expect(link.text()).to.equal(pageNumber);
-      }
-    });
+  it('should not show dropdown on initial load', () => {
+    expect(megaMenu.find('.vetnav-level2').exists()).to.be.false;
   });
 
-  it('should show the correct range of pages if the max is exceeded', () => {
-    const tree = shallow(
-      <Pagination
-        {...props}
-        page={5}
-        pages={20}/>
-    );
-    const links = tree.find('a');
-    expect(links).to.have.length(12);
-    links.forEach((link, index) => {
-      if (index === 0) {
-        expect(link.text()).to.equal('Prev');
-      } else if (index === 11) {
-        expect(link.text()).to.equal('Next');
-      } else {
-        const pageNumber = (index + 4).toString();
-        expect(link.text()).to.equal(pageNumber);
-      }
+  it('should show dropdown if state.currentDropdown is = menu title', () => {
+    megaMenu.setProps({
+      ...megaMenu.props(),
+      currentDropdown: 'Nav Title 1',
     });
+
+    expect(megaMenu.find('.vetnav-level2').first().text()).to.equal('Section Title 1');
   });
 
-  it('show the last page if enabled and there are more pages than max', () => {
-    const tree = shallow(
-      <Pagination
-        {...props}
-        page={3}
-        pages={15}
-        maxPageListLength={10}
-        showLastPage/>
-    );
-    const links = tree.find('a');
-    expect(links).to.have.length(12);
-    links.forEach((link, index) => {
-      if (index === 0) {
-        expect(link.text()).to.equal('Prev');
-      } else if (index === 9) {
-        expect(link.text()).to.equal('...');
-      } else if (index === 10) {
-        expect(link.text()).to.equal('15');
-      } else if (index === 11) {
-        expect(link.text()).to.equal('Next');
-      } else {
-        const pageNumber = (index + 2).toString();
-        expect(link.text()).to.equal(pageNumber);
-      }
+  it('should run updateCurrentSection if menu section is clicked on', () => {
+    const spy = sinon.spy(megaMenu.instance(), 'updateCurrentSection');
+
+    megaMenu.setProps({
+      currentDropdown: 'Nav Title 1',
     });
+
+    megaMenu.find('.vetnav-level2').at(1).simulate('click');
+
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.getCall(0).args[0]).to.equal('Section Title 2');
   });
 
-  it('should show a continuous list when within range of the last page', () => {
-    const tree = shallow(
-      <Pagination
-        {...props}
-        page={6}
-        pages={15}
-        maxPageListLength={10}
-        showLastPage/>
-    );
-    const links = tree.find('a');
-    expect(links).to.have.length(12);
-    links.forEach((link, index) => {
-      if (index === 0) {
-        expect(link.text()).to.equal('Prev');
-      } else if (index === 10) {
-        expect(link.text()).to.equal('15');
-      } else if (index === 11) {
-        expect(link.text()).to.equal('Next');
-      } else {
-        const pageNumber = (index + 5).toString();
-        expect(link.text()).to.equal(pageNumber);
-      }
+  it.only('should show Nav Title 1 section if state.currentSection is = Section Title 2', () => {
+    megaMenu.setProps({
+      ...megaMenu.props(),
+      currentDropdown: 'Nav Title 1',
+      currentSection: 'Section Title 2',
     });
+
+    expect(megaMenu.find('h3').at(0).text()).to.equal('Menu Item 1');
   });
 
-  it('should pass aXe check', () => {
-    return axeCheck(<Pagination
-      {...props}
-      page={3}
-      pages={5}/>
+  it('should pass axe check when open', () => {
+    return axeCheck(
+      <MegaMenu
+        data={data}
+        handleOnClick={(title) => { title; }}
+        updateCurrentSection={(title) => { title; }}></MegaMenu>
     );
   });
 });
