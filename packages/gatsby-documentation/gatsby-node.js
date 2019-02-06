0520ef874baa69448f8cd3a7d83a2f3f01cd4954
Bill Tran <bill@billjohntran.com>
2019-02-06 14:05:08 -0800

    add this commit to rebuild

--- packages/gatsby-documentation/gatsby-node.js
+++ packages/gatsby-documentation/gatsby-node.js
@@ -68,6 +68,7 @@ exports.createPages = ({ graphql, actions }) => {
           }
         `
       ).then(result => {
+
         if (result.errors) {
           console.log(result.errors)
           reject(result.errors)
