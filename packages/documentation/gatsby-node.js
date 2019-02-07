7ddfbebb7f750d305166ca82c5a09b95f3ac7794
Bill Tran <bill@billjohntran.com>
2019-02-06 17:54:35 -0800

    edited the how to file

--- packages/documentation/gatsby-node.js
+++ packages/documentation/gatsby-node.js
@@ -68,7 +68,6 @@ exports.createPages = ({ graphql, actions }) => {
           }
         `
       ).then(result => {
-
         if (result.errors) {
           console.log(result.errors)
           reject(result.errors)
