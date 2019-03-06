efb84d85305115600aa69fe1338d78c78ccb7cc2
Jeff Balboni <jeff@adhocteam.us>
2019-03-06 09:59:10 -0500

    Fix path

--- packages/documentation/gatsby-config.js
+++ packages/documentation/gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteMetadata: {
     title: `VA.gov Developer's Documentations`,
     description: `Resources and documention for the Development within the VA.gov project`,
-    infoArchitecture: require('./information-architecture.js')
+    sidebar: require('./src/sidebar.js')
   },
   pathPrefix: '/veteran-facing-services-tools',
   plugins: [
