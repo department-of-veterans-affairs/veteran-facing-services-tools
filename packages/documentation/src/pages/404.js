568fffdd9104170062d0542eeffd589ef7e60859
Jeff Balboni <jeff@adhocteam.us>
2019-03-06 10:27:43 -0500

    Update 404

--- packages/documentation/src/pages/404.js
+++ packages/documentation/src/pages/404.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import Layout from '../layouts/layout';
 
-const NotFoundPage = () => (
-  <Layout>
+const NotFoundPage = ({ location }) => (
+  <Layout location={location}>
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
