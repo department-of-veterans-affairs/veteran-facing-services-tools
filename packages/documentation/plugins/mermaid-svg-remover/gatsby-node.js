67fde20327747c0fea780360cdfb7191671f720e
Bill Tran <bill@billjohntran.com>
2019-03-18 13:33:15 -0700

    restored yarn.lock and removed package-lock.json add async/await to fs.unlink

--- packages/documentation/plugins/mermaid-svg-remover/gatsby-node.js
+++ packages/documentation/plugins/mermaid-svg-remover/gatsby-node.js
@@ -5,21 +5,21 @@ exports.onPreBootstrap = async () => {
   const staticDir = path.join(__dirname, '../../static/mermaid-svgs');
   const publicDir = path.join(__dirname, '../../public/mermaid-svgs');
 
-  await fs.readdir(staticDir, (err, files) => {
+  await fs.readdir(staticDir, async (err, files) => {
     if (err) throw err;
 
     for (const file of files) {
-      fs.unlink(path.join(staticDir, file), err => {
+      await fs.unlink(path.join(staticDir, file), err => {
         if (err) throw err;
       });
     }
   });
 
-  await fs.readdir(publicDir, (err, files) => {
+  await fs.readdir(publicDir, async (err, files) => {
     if (err) throw err;
 
     for (const file of files) {
-      fs.unlink(path.join(publicDir, file), err => {
+      await fs.unlink(path.join(publicDir, file), err => {
         if (err) throw err;
       });
     }
