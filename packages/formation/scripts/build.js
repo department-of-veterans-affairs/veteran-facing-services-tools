ca774d6891d781ee4151004331508e0c242993c4
Rian Fowler <rian.fowler@adhocteam.us>
2019-03-27 15:37:50 -0600

    update webpack config and build scirpt

--- packages/formation/scripts/build.js
+++ packages/formation/scripts/build.js
@@ -1,17 +1,11 @@
 /* eslint-disable no-console */
-const mkdirp = require('mkdirp');
-const fs = require('fs-extra');
 const webpack = require('webpack');
 const webpackConfig = require('../../../webpack.config.prod.js');
 
 console.log('Starting Formation build');
 console.log('Cleaning old build');
-mkdirp.sync('./dist/img');
-mkdirp.sync('./dist/fonts');
 
-const compiler = webpack(webpackConfig);
-
-compiler.run((err, stats) => {
+webpack(webpackConfig, (err, stats) => {
   if (err || stats.hasErrors()) {
     const info = stats.toJson();
     console.error(info.errors);
@@ -19,6 +13,3 @@ compiler.run((err, stats) => {
   }
   console.log(stats.toString('minimal'));
 });
-
-fs.copySync('./assets/img', './dist/img');
-fs.copySync('./assets/fonts', './dist/fonts');
