ff240d263b3743b53fbf5d115bd522231f2d7a4d
Bill Tran <bill@billjohntran.com>
2019-04-02 23:43:50 -0700

    add accordian js

---
+++ packages/formation/js/accordian.js
@@ -0,0 +1,29 @@
+const toBoolean = (value) => {
+  return value === 'true';
+};
+
+const loadAccordianHandler = function(){
+  const usaAccordian = document.getElementsByClassName('usa-accordion')[0];
+
+  usaAccordian.addEventListener('click', (e) => {
+    const target = e.target;
+
+    if (target.getAttribute('aria-controls')) {
+      const dropDownElement = document.getElementById(target.getAttribute('aria-controls'));
+      const dropDownHiddenAttr = dropDownElement.getAttribute('aria-hidden');
+      const targetExpandedAttr = target.getAttribute('aria-expanded');
+
+      dropDownElement.setAttribute('aria-hidden', !toBoolean(dropDownHiddenAttr));
+      target.setAttribute('aria-expanded', !toBoolean(targetExpandedAttr));
+    }
+  });
+};
+
+if (
+    document.readyState === "complete" ||
+    (document.readyState !== "loading" && !document.documentElement.doScroll)
+) {
+  loadAccordianHandler();
+} else {
+  document.addEventListener("DOMContentLoaded", loadAccordianHandler);
+}
