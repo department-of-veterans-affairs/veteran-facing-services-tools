a01606b6a0bf00e47696e8479d96c9bf5d403fad
Bill Tran <bill@billjohntran.com>
2019-04-03 23:33:08 -0700

    modified accordian to handle vets-website

--- packages/formation/js/accordian.js
+++ packages/formation/js/accordian.js
@@ -1,29 +1,41 @@
 const toBoolean = value => value === 'true';
 
 const loadAccordianHandler = () => {
-  const usaAccordian = document.getElementsByClassName('usa-accordion')[0];
+  const usaAccordion = document.getElementsByClassName('usa-accordion');
+  const usaAccordionContentElements = [
+    ...document.getElementsByClassName('usa-accordion-content'),
+  ];
 
-  usaAccordian.addEventListener('click', e => {
-    const target = e.target;
-
-    if (target.getAttribute('aria-controls')) {
-      const dropDownElement = document.getElementById(
-        target.getAttribute('aria-controls'),
-      );
-      const dropDownHiddenAttr = dropDownElement.getAttribute('aria-hidden');
-      const targetExpandedAttr = target.getAttribute('aria-expanded');
-
-      dropDownElement.setAttribute(
+  usaAccordionContentElements.forEach(el => {
+    if (el.getAttribute('aria-hidden') === null) {
+      el.setAttribute(
         'aria-hidden',
-        !toBoolean(dropDownHiddenAttr),
+        !el.previousElementSibling.getAttribute('aria-expanded'),
       );
-      target.setAttribute('aria-expanded', !toBoolean(targetExpandedAttr));
     }
   });
+
+  for (let i = 0; i < usaAccordion.length; i++) {
+    usaAccordion[i].addEventListener('click', e => {
+      const target = e.target;
+
+      if (target.getAttribute('aria-controls')) {
+        const dropDownElement = document.getElementById(
+          target.getAttribute('aria-controls'),
+        );
+        const targetExpandedAttr = toBoolean(
+          target.getAttribute('aria-expanded'),
+        );
+
+        dropDownElement.setAttribute('aria-hidden', targetExpandedAttr);
+        target.setAttribute('aria-expanded', !targetExpandedAttr);
+      }
+    });
+  }
 };
 
 if (
-  document.readyState === 'complete' ||
+  document.readyState === 'complete' &&
   (document.readyState !== 'loading' && !document.documentElement.doScroll)
 ) {
   loadAccordianHandler();
