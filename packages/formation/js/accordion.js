08dc2df67ce892930279be32fbbfa42f26f75f14
Bill Tran <bill@billjohntran.com>
2019-04-05 06:36:27 -0700

    add code to address uswds accordion tests

--- packages/formation/js/accordion.js
+++ packages/formation/js/accordion.js
@@ -18,6 +18,13 @@ const loadAccordianHandler = () => {
   for (let i = 0; i < usaAccordion.length; i++) {
     usaAccordion[i].addEventListener('click', e => {
       const target = e.target;
+      const multiSelectable = toBoolean(
+        usaAccordion[i].getAttribute('aria-multiselectable'),
+      );
+      const expandedEl = e.currentTarget.querySelector(
+        '[aria-expanded="true"]',
+      );
+      const hiddenEl = e.currentTarget.querySelector('[aria-hidden="false"]');
 
       if (target.getAttribute('aria-controls')) {
         const dropDownElement = document.getElementById(
@@ -29,6 +36,13 @@ const loadAccordianHandler = () => {
 
         dropDownElement.setAttribute('aria-hidden', targetExpandedAttr);
         target.setAttribute('aria-expanded', !targetExpandedAttr);
+      }
+
+      if (!multiSelectable) {
+        if (expandedEl !== null) {
+          expandedEl.setAttribute('aria-expanded', false);
+          hiddenEl.setAttribute('aria-hidden', true);
+        }
       }
     });
   }
