a9ab7fa94c6e9fe99aa2d82bcf33c4a5fb469322
Rian Fowler <rian.fowler@adhocteam.us>
2019-04-25 09:33:38 -0600

    use forked version of library

--- packages/documentation/gatsby-config.js
+++ packages/documentation/gatsby-config.js
@@ -67,22 +67,20 @@ module.exports = {
     `github-api-pages`,
     `mdx-pages`,
     {
-      resolve: `@andrew-codes/gatsby-plugin-elasticlunr-search`,
+      resolve: `@gatsby-contrib/gatsby-plugin-elasticlunr-search`,
       options: {
-          // Fields to index
-          fields: [
-              'title',
-              'keywords',
-          ],
-          // How to resolve each field's value for a supported node type
-          resolvers: {
-              // For any node of type MarkdownRemark, list how to resolve the fields' values
-              MarkdownRemark: {
-                  title: node => node.frontmatter.title,
-                  keywords: node => node.frontmatter.keywords,
-              },
+        // Fields to index
+        fields: [`title`, `tags`],
+        // How to resolve each field`s value for a supported node type
+        resolvers: {
+          // For any node of type MarkdownRemark, list how to resolve the fields` values
+          MarkdownRemark: {
+            title: node => node.frontmatter.title,
+            tags: node => node.frontmatter.tags,
+            path: node => node.frontmatter.path,
           },
+        },
       },
-  },
+    },
   ],
 }
