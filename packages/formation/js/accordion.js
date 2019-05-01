a1296bfbee2b2f1bee98f2072dd2d3de90dd56ab
Bill Tran <bill@billjohntran.com>
2019-05-01 07:07:57 -0700

    lint fix

--- packages/formation/js/accordion.js
+++ packages/formation/js/accordion.js
@@ -16,67 +16,63 @@ const isElementInViewport = (
 };
 
 const loadAccordionHandler = () => {
-  const usaAccordion = [
-    ...document.getElementsByClassName('usa-accordion'),
-    ...document.getElementsByClassName('usa-accordion-bordered'),
-  ];
-  const usaAccordionContentElements = [
-    ...document.getElementsByClassName('usa-accordion-content'),
-  ];
-
-  usaAccordionContentElements.forEach(el => {
-    if (el.getAttribute('aria-hidden') === null) {
-      const buttonElement = el.parentElement.querySelector(
-        `[aria-controls="${el.id}"]`,
-      );
-
-      el.setAttribute(
-        'aria-hidden',
-        !toBoolean(buttonElement.getAttribute('aria-expanded')),
-      );
-    }
-  });
-
-  usaAccordion.forEach(element => {
-    element.addEventListener('click', e => {
-      const target = e.target;
-      const other = Array.from(
-        element.getElementsByClassName('usa-accordion-button'),
-      ).filter(item => item !== target);
-
-      const multiSelectable = toBoolean(
-        element.getAttribute('aria-multiselectable'),
-      );
-
-      if (target.getAttribute('aria-controls')) {
-        if (!multiSelectable) {
-          other.forEach(el => {
-            const contentEl = el.getAttribute('aria-controls');
-
-            el.setAttribute('aria-expanded', 'false');
-
-            document
-              .getElementById(contentEl)
-              .setAttribute('aria-hidden', 'true');
-          });
-        }
-
-        const dropDownElement = document.getElementById(
-          target.getAttribute('aria-controls'),
+  document
+    .querySelectorAll('.usa-accordion-content:not([aria-hidden])')
+    .forEach(el => {
+      if (el.getAttribute('aria-hidden') === null) {
+        const buttonElement = el.parentElement.querySelector(
+          `[aria-controls="${el.id}"]`,
         );
-        const targetExpandedAttr = toBoolean(
-          target.getAttribute('aria-expanded'),
+        const hiddenValue = !toBoolean(
+          buttonElement.getAttribute('aria-expanded'),
         );
 
-        dropDownElement.setAttribute('aria-hidden', targetExpandedAttr);
-        target.setAttribute('aria-expanded', !targetExpandedAttr);
-
-        if (!isElementInViewport(target)) {
-          element.scrollIntoView();
-        }
+        el.setAttribute('aria-hidden', hiddenValue);
       }
     });
-  });
+
+  document
+    .querySelectorAll('.usa-accordion, .usa-accordion-bordered')
+    .forEach(element => {
+      element.addEventListener('click', e => {
+        const target = e.target;
+        const other = Array.from(
+          element.getElementsByClassName('usa-accordion-button'),
+        ).filter(item => item !== target);
+
+        const multiSelectable = toBoolean(
+          element.getAttribute('aria-multiselectable'),
+        );
+
+        if (target.getAttribute('aria-controls')) {
+          if (!multiSelectable) {
+            other.forEach(el => {
+              const contentEl = el.getAttribute('aria-controls');
+
+              el.setAttribute('aria-expanded', 'false');
+
+              document
+                .getElementById(contentEl)
+                .setAttribute('aria-hidden', 'true');
+            });
+          }
+
+          const dropDownElement = document.getElementById(
+            target.getAttribute('aria-controls'),
+          );
+          const targetExpandedAttr = toBoolean(
+            target.getAttribute('aria-expanded'),
+          );
+
+          dropDownElement.setAttribute('aria-hidden', targetExpandedAttr);
+          target.setAttribute('aria-expanded', !targetExpandedAttr);
+
+          if (!isElementInViewport(target)) {
+            element.scrollIntoView();
+          }
+        }
+      });
+    });
 };
 
 export default loadAccordionHandler;
