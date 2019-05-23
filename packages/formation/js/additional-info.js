30a0c1414bc13b5815c05ca1fb57b7ea3aa513bd
Rian Fowler <rian.fowler@adhocteam.us>
2019-05-23 08:42:11 -0600

    move additional info component and tests from vets-website

---
+++ packages/formation/js/additional-info.js
@@ -0,0 +1,60 @@
+function uniqueId() {
+  return Math.floor(Math.random() * Math.floor(100000));
+}
+
+function qSA(rootNode, selector) {
+  return Array.from(rootNode.querySelectorAll(selector));
+}
+
+function qS(rootNode, selector) {
+  return rootNode.querySelector(selector);
+}
+
+function removeChildNodes(node) {
+  while (node.firstChild) {
+    node.removeChild(node.firstChild);
+  }
+}
+
+export default function createAdditionalInfoWidget() {
+  const widgets = qSA(document, '.additional-info-container');
+
+  if (widgets.length) {
+    console.log(widgets.length);
+    widgets.forEach(el => {
+      const titleNode = qS(el, '.additional-info-title');
+      const titleText =
+        (titleNode && titleNode.textContent) || 'More information';
+        console.log(titleText);
+      const contentMarkup = qS(el, '.additional-info-content').innerHTML;
+      const additionalInfoId = uniqueId('additional-info-');
+
+      const template = `
+          <button type="button" class="additional-info-button va-button-link" aria-controls="${additionalInfoId}" aria-expanded="false">
+            <span class="additional-info-title">${titleText}
+              <i class="fa fa-angle-down"></i>
+            </span>
+          </button>
+          <span id="${additionalInfoId}">
+            <div class="additional-info-content">
+              ${contentMarkup}
+            </div>
+          </span>`;
+
+      removeChildNodes(el);
+
+      el.insertAdjacentHTML('afterbegin', template);
+
+      const chevron = qS(el, 'i.fa-angle-down');
+      const button = qS(el, 'button');
+
+      button.addEventListener('click', () => {
+        const ariaExpanded = JSON.parse(button.getAttribute('aria-expanded'));
+
+        button.setAttribute('aria-expanded', `${!ariaExpanded}`);
+        button.parentNode.classList.toggle('form-expanding-group-open');
+        chevron.classList.toggle('open');
+      });
+    });
+  }
+}
