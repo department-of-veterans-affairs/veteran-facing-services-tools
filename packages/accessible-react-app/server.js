e22871332582a4a8e87e4000fcb24863e8406d46
Trevor Pierce <trevor@adhocteam.us>
2019-09-23 17:28:30 -0500

    First commit of Accessible React App demo.

---
+++ packages/accessible-react-app/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+// Serve static assets normally
+app.use(express.static(path.resolve(__dirname, 'dist')));
+
+// Handle every other route with index.html, which contains
+// the application's ReactDOM.render() method.
+app.get('*', (request, response) => {
+  response.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port);
+console.log(`Server started on port ${port}`);
---
+++ packages/accessible-react-app/src/js/layout/Layout.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
+import { MAIN_LAYOUT_ID } from '../config/constants';
+
+import Header from '../components/Header';
+import SkipNav from '../components/Skipnav';
+
+const Layout = props => {
+  const { children, title } = props;
+
+  return (
+    <>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+      <SkipNav text="Skip to main content" />
+      <Header />
+      <div id={MAIN_LAYOUT_ID}>{children}</div>
+    </>
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default Layout;
