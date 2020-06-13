e293b60697891464d25a1e936662c17ca8ac3856
Theo Bentum <theo@adhocteam.us>
2020-06-12 17:06:01 -0700

    Remove gatsby-transformer-sharp

--- packages/documentation/gatsby-config.js
+++ packages/documentation/gatsby-config.js
@@ -28,7 +28,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    // `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
