f2dbf016583e31e556ab2bc810908ca518e09bad
Alex Pappas <alex.pappas@oddball.io>
2020-08-24 10:55:53 -0700

    changing to production only

--- packages/documentation/gatsby-config.js
+++ packages/documentation/gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
       // Include GTM in development.
       //
       // Defaults to false meaning GTM will only be loaded in production.
-      includeInDevelopment: true,
+      includeInDevelopment: false,
 
       // datalayer to be set before GTM is loaded
       // should be an object or a function that is executed in the browser
