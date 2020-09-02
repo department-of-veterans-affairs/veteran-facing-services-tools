4289732fcd0e1bfae4adc45217e0170b0b7d78b3
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2020-09-01 20:44:53 -0400

    Don't show edit link if there is no source URL.

--- packages/documentation/src/components/EditLink.jsx
+++ packages/documentation/src/components/EditLink.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 export default function EditLink({ sourceUrl }) {
-  return (
+  return sourceUrl ? (
     <div>
       <i class="fa fa-edit" aria-hidden="true" />{' '}
       <a href={sourceUrl}>Edit this page on GitHub</a>
     </div>
-  );
+  ) : null;
 }
