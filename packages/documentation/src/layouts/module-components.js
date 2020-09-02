af639759ceab08aa1915fa62ed0c78faf4fa161e
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2020-09-02 09:11:54 -0400

    Refactored ModuleComponentsLayout into a function component.

--- packages/documentation/src/layouts/module-components.js
+++ packages/documentation/src/layouts/module-components.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { graphql } from 'gatsby';
 import { MDXProvider } from '@mdx-js/react';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
@@ -14,61 +14,59 @@ const MyCodeComponent = ({ children }) => (
   </LiveProvider>
 );
 
-export default class ModuleComponentsLayout extends Component {
-  sortedRequiredProps() {
-    return this.props.data.componentMetadata.childrenComponentProp.sort(a =>
-      a.required ? -1 : 1
-    );
+export default function ModuleComponentsLayout({
+  children,
+  data,
+  location,
+  pageContext,
+  tableOfContents,
+}) {
+  const {
+    childrenComponentProp,
+    displayName,
+    docblock,
+  } = data.componentMetadata;
+
+  const sortedRequiredProps =
+    childrenComponentProp.sort(a => a.required ? -1 : 1);
+
+  const jsDocBlocks = docblock && docblock.split(/\r?\n/);
+  const mdxCodeBlock = data.mdx.body;
+
+  // Window is not available during build, so this was added deal with making
+  // window available to some components.
+  // Tried to add it to componentDidMount but it didn't work correctly.
+  if (typeof window !== 'undefined') {
+    window.dataLayer = [];
+    window.VetsGov = {};
   }
 
-  render() {
-    const {
-      children,
-      data,
-      location,
-      pageContext,
-      tableOfContents,
-    } = this.props;
-
-    const { displayName, docblock } = data.componentMetadata;
-    const jsDocBlock = docblock;
-    const jsDockBlocks = jsDocBlock.split(/\r?\n/);
-    const mdxCodeBlock = data.mdx.body;
-    // Window is not available during build, so this was added deal with making
-    // window available to some components.
-    // Tried to add it to componentDidMount but it didn't work correctly.
-    if (typeof window !== 'undefined') {
-      window.dataLayer = [];
-      window.VetsGov = {};
-    }
-
-    return (
-      <MDXProvider components={{ code: MyCodeComponent }}>
-        <SidebarLayout location={location} pageContext={pageContext}>
-          <div className="content">
-            {children}
-            <h2>{displayName}</h2>
-            {jsDocBlock && (
-              <div>
-                <h3>JsDocs:</h3>
-                <div className="site-c-reactcomp__rendered">
-                  {jsDockBlocks.map((block, i) => (
-                    <p key={`${block}-${i}`}>{block}</p>
-                  ))}
-                </div>
+  return (
+    <MDXProvider components={{ code: MyCodeComponent }}>
+      <SidebarLayout location={location} pageContext={pageContext}>
+        <div className="content">
+          {children}
+          <h2>{displayName}</h2>
+          {jsDocBlocks ? (
+            <div>
+              <h3>JsDocs:</h3>
+              <div className="site-c-reactcomp__rendered">
+                {jsDocBlocks.map((block, i) => (
+                  <p key={`${block}-${i}`}>{block}</p>
+                ))}
               </div>
-            )}
+            </div>
+          ) : null}
 
-            <MDXRenderer tableOfContents={tableOfContents}>
-              {mdxCodeBlock}
-            </MDXRenderer>
-            <h2 style={{ marginTop: '2rem' }}>Props:</h2>
-            <PropsTable propMetaData={this.sortedRequiredProps()} />
-          </div>
-        </SidebarLayout>
-      </MDXProvider>
-    );
-  }
+          <MDXRenderer tableOfContents={tableOfContents}>
+            {mdxCodeBlock}
+          </MDXRenderer>
+          <h2 style={{ marginTop: '2rem' }}>Props:</h2>
+          <PropsTable propMetaData={sortedRequiredProps} />
+        </div>
+      </SidebarLayout>
+    </MDXProvider>
+  );
 }
 
 export const pageQuery = graphql`
