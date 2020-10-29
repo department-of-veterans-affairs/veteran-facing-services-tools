6eb6c6e94089c9ba61c63d8da63ba5feacbf3106
Craig Wheeler <craig@craigwheeler.net>
2020-10-29 17:34:30 -0400

    remove unused argTypes

--- packages/formation-react/src/components/ErrorableCheckbox/ErrorableCheckbox.stories.jsx
+++ packages/formation-react/src/components/ErrorableCheckbox/ErrorableCheckbox.stories.jsx
@@ -4,14 +4,6 @@ import ErrorableCheckbox from './ErrorableCheckbox';
 export default {
   title: 'Library/ErrorableCheckbox',
   component: ErrorableCheckbox,
-  argTypes: {
-    required: { control: 'boolean' },
-    errorMessage: { control: 'text' },
-    id: { control: 'text' },
-    title: { control: 'text' },
-    label: { control: 'text' },
-    labelAboveCheckbox: { control: 'text' },
-  },
 };
 
 const Template = (args) => <ErrorableCheckbox {...args} />;
@@ -19,8 +11,6 @@ const Template = (args) => <ErrorableCheckbox {...args} />;
 const defaultArgs = {
   required: true,
   errorMessage: 'Error message',
-  id: 'default',
-  title: 'ErrorableCheckbox',
   label: "I want to type in my school's name and address.",
   labelAboveCheckbox:
     "If you don't find your school in the search results, then check the box to enter in your school information manually.",
