d53cf730a21afdcf727e2036b1232637b89735a7
Craig Wheeler <craig@craigwheeler.net>
2020-10-30 16:36:46 -0400

    create error story

--- packages/formation-react/src/components/ErrorableFileInput/ErrorableFileInput.stories.jsx
+++ packages/formation-react/src/components/ErrorableFileInput/ErrorableFileInput.stories.jsx
@@ -10,14 +10,18 @@ const Template = (args) => <ErrorableFileInput {...args} />;
 
 const defaultArgs = {
   buttonText: 'Upload some files',
-  errorMessage: 'Error message',
   onChange: () => {
     alert('uploaded file');
   },
 };
 
 export const Default = Template.bind({});
-
 Default.args = {
   ...defaultArgs,
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  ...defaultArgs,
+  errorMessage: 'Error message',
 };
