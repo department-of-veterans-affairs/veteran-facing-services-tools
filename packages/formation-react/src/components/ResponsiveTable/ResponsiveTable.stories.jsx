cb8b9bd484b3edcd6ff65649ea20ebeac2e24746
Brooks Johnson <brooks.johnson@pm.me>
2020-10-30 14:35:39 -0700

    Correct the story

--- packages/formation-react/src/components/ResponsiveTable/ResponsiveTable.stories.jsx
+++ packages/formation-react/src/components/ResponsiveTable/ResponsiveTable.stories.jsx
@@ -5,35 +5,31 @@ import ResponsiveTable from './ResponsiveTable';
 export default {
   title: 'Library/ResponsiveTable',
   component: ResponsiveTable,
-  argTypes: {
-    columns: {
-      control: {
-        type: 'array',
-      },
-    },
-    data: { control: 'array' },
-    tableClass: {
-      control: {
-        type: 'text',
-      },
-    },
-  },
 };
 
 const Template = (args) => <ResponsiveTable {...args} />;
-const row1Data = {
-  'Column 1': 'test',
-  'Column 2': 'testt',
-  rowClassName: 'row-class-name',
-  key: 'row-key',
-};
-
-const data = [row1Data, { ...row1Data, column2Header: 'testt2' }];
 
 const defaultArgs = {
-  columns: ['Column 1', 'Column 2'],
-  data,
-  tableClass: 'Alert title',
+  data: [
+    {
+      column1: 'Testing',
+      column2: 'Moooore testing',
+    },
+    {
+      column1: 'Second row',
+      column2: 'Some things',
+    },
+  ],
+  fields: [
+    {
+      label: 'Column 1',
+      value: 'column1',
+    },
+    {
+      label: 'Column 2',
+      value: 'column2',
+    },
+  ],
 };
 
 /**
