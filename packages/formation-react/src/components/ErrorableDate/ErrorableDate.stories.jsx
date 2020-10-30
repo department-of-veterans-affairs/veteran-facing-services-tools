d4161b7770e749efb9c5d77acbda50ea4358b754
Craig Wheeler <craig@craigwheeler.net>
2020-10-30 17:09:07 -0400

    created error story

--- packages/formation-react/src/components/ErrorableDate/ErrorableDate.stories.jsx
+++ packages/formation-react/src/components/ErrorableDate/ErrorableDate.stories.jsx
@@ -4,38 +4,16 @@ import ErrorableDate from './ErrorableDate';
 export default {
   title: 'Library/ErrorableDate',
   component: ErrorableDate,
-  argTypes: {
-    required: { control: 'boolean' },
-    label: { control: 'text' },
-    name: { control: 'text' },
-    requiredMessage: { control: 'text' },
-    invalidMessage: { control: 'text' },
-  },
 };
 
 const Template = (args) => {
-  const [date, setDate] = useState({
-    day: {
-      value: '14',
-      dirty: false,
-    },
-    month: {
-      value: '4',
-      dirty: false,
-    },
-    year: {
-      value: '1983',
-      dirty: false,
-    },
-  });
+  const [date, setDate] = useState(args.date);
   return (
-    <div>
-      <ErrorableDate
-        {...args}
-        date={date}
-        onValueChange={(date) => setDate(date)}
-      />
-    </div>
+    <ErrorableDate
+      {...args}
+      date={date}
+      onValueChange={(date) => setDate(date)}
+    />
   );
 };
 
@@ -44,11 +22,42 @@ const defaultArgs = {
   label: 'Please tell us a date',
   name: 'Errorable Date',
   requiredMessage: 'Please provide a response',
-  invalidMessage: 'Please provide a valid date',
+  date: {
+    day: {
+      value: '1',
+      dirty: false,
+    },
+    month: {
+      value: '4',
+      dirty: false,
+    },
+    year: {
+      value: '1976',
+      dirty: false,
+    },
+  },
 };
 
 export const Default = Template.bind({});
-
 Default.args = {
   ...defaultArgs,
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  ...defaultArgs,
+  date: {
+    day: {
+      value: '1',
+      dirty: true,
+    },
+    month: {
+      value: '4',
+      dirty: true,
+    },
+    year: {
+      value: '0000',
+      dirty: true,
+    },
+  },
 };
