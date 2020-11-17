dad4941c93d4b9bc7c84caea484169ab733d72d7
Brooks Johnson <brooks.johnson@pm.me>
2020-11-17 09:58:42 -0800

    Try to do a diff

--- scripts/publish-storybook.js
+++ scripts/publish-storybook.js
@@ -1,2 +1,14 @@
+const exec = require('child_process').exec;
+
+function execute(command, callback) {
+  exec(command, (error, stdout, stderr) => {
+    callback(stdout);
+  });
+}
+
+execute(`git diff --name-only HEAD ${process.env.LAST_COMMIT}`, output => {
+  console.log(output);
+});
+
 console.log('Testing');
 console.log(process.env.CIRCLE_COMPARE_URL);
