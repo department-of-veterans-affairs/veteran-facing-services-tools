8471692fd20260c17cc19eb3d8a61061d2fa1390
Christopher Valarida <chris.valarida@adhocteam.us>
2020-12-10 10:22:44 -0800

    Fix the pre-publish script

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -6,14 +6,16 @@
 const { execSync } = require('child_process');
 const chalk = require('chalk');
 
-const branchName = execSync('git branch --show-current');
+const branchName = execSync('git branch --show-current')
+  .toString('utf8')
+  .trim();
 
 if (branchName !== 'master') {
   console.log(
     chalk.yellow(
       `You're currently on branch ${chalk.cyan(
         branchName,
-      )}Please check out ${chalk.cyan('master')} before publishing.\n`,
+      )}\nPlease check out ${chalk.cyan('master')} before publishing.\n`,
     ),
   );
   process.exit(1);
