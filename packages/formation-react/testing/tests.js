24d026abaa1716535d3a1aa1620a717931c7ebe4
Brooks Johnson <brooks.johnson@pm.me>
2021-01-28 10:44:40 -0800

    Remove formation-react dir except README

--- packages/formation-react/testing/tests.js
+++
@@ -1,9 +0,0 @@
-import 'axe-core';
-
-const Enzyme = require('enzyme');
-const Adapter = require('enzyme-adapter-react-16');
-
-Enzyme.configure({ adapter: new Adapter() });
-
-const testsContext = require.context('..', true, /\.spec\.js[x]{0,1}$/);
-testsContext.keys().forEach(testsContext);
