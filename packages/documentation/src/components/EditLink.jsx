abf135585b0e2cd74ad9e9bbf8e8c2901d2f2c2d
Tim Wright <timwright12@gmail.com>
2021-07-15 15:32:50 -0400

    save

--- packages/documentation/src/components/EditLink.jsx
+++ packages/documentation/src/components/EditLink.jsx
@@ -6,5 +6,6 @@ export default function EditLink({ sourceUrl }) {
       <i class="fa fa-edit" aria-hidden="true" />{' '}
       <a href={sourceUrl}>Edit this page on GitHub</a>
     </div>
+    Hello
   ) : null;
 }
