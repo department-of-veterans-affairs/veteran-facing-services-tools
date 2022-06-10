db6da2d3224f22a5728e89f789d9892b7369f02a
Andrés Rivera Toro <andres.rivera.toro@gmail.com>
2022-06-10 16:14:41 -0400

    Deprecate green button (#830)
    * feat: deprecrate green button
    
    * chore: fix value check
    
    * test: add design system rules tests
    
    * chore: fix typo
    
    * chore: increment minor version
    
    * chore: rename to deprecated-classes

---
+++ packages/eslint-plugin/lib/rules/deprecated-classes.js
@@ -0,0 +1,31 @@
+const getPropNode = (node, propName) =>
+  node.openingElement.attributes.find(n => n.name.name === propName);
+
+module.exports = {
+  meta: {
+    docs: {
+      description: 'VA.gov Design System deprecated classes',
+      category: 'Best Practices',
+      recommended: false,
+    },
+    type: 'suggestion',
+  },
+
+  create(context) {
+    return {
+      JSXElement(node) {
+        /**
+         * Deprecate green button
+         */
+        const className = getPropNode(node, 'className');
+        if (className?.value.value.includes('va-button-primary')) {
+          context.report({
+            node,
+            message:
+              'The va-button-primary utility class is deprecated. Please visit https://design.va.gov/components/button/ for our guidance on buttons.',
+          });
+        }
+      },
+    };
+  },
+};
