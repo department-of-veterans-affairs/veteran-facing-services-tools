45cf84516c6e1cf72ab9c9491e3dbf0a6d91c27e
Brooks Johnson <brooks.johnson@pm.me>
2022-08-23 15:48:40 -0700

    Add new rule to checking contact prop length
    Since the autofixable rule is going to be re-writing code to use the
    va-telephone component, we want to be able to notify developers when the
    contact prop doesn't match the expectations.
    
    This is a separate rule since the ESLint best practices [1] say to keep
    the fixes as small as possible. Having one rule rewrite code to use a
    `<va-telephone>` instead of an `<a>` _and_ also having it rewrite the
    value of the contact prop by possibly removing a number seemed like a
    bad idea.
    
    - 1. https://eslint.org/docs/latest/developer-guide/working-with-rules#applying-fixes

---
+++ packages/eslint-plugin/lib/rules/telephone-contact-3-or-10-digits.js
@@ -0,0 +1,38 @@
+const jsxAstUtils = require('jsx-ast-utils');
+
+const { elementType, getProp, getLiteralPropValue } = jsxAstUtils;
+
+const MESSAGE =
+  'The <va-telephone> contact prop should only contain digits and be 3 or 10 digits long.';
+
+module.exports = {
+  meta: {
+    type: 'suggestion',
+    fixable: 'code',
+  },
+
+  create(context) {
+    return {
+      JSXOpeningElement(node) {
+        // Exit early if we aren't on a telephone component
+        if (elementType(node) !== 'va-telephone') return;
+
+        const contactProp = getProp(node.attributes, 'contact');
+        const contactValue = getLiteralPropValue(contactProp);
+
+        // If contactValue is undefined then the first part of the expression will
+        // fail, since undefined doesn't equal 3 or 10
+        if (
+          contactValue &&
+          contactValue?.length !== 3 &&
+          contactValue?.length !== 10
+        ) {
+          context.report({
+            node,
+            message: MESSAGE,
+          });
+        }
+      },
+    };
+  },
+};
