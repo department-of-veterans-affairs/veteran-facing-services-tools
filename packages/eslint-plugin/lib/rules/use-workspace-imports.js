5fcb4f984ab183871fe3f9e910453867253decdd
Ian <harrison.ian.t@gmail.com>
2022-09-13 13:21:08 -0400

    update eslint rule now that all platform dirs have workspaces

--- packages/eslint-plugin/lib/rules/use-workspace-imports.js
+++ packages/eslint-plugin/lib/rules/use-workspace-imports.js
@@ -1,39 +1,23 @@
-let MESSAGE = 'Import modules from platform using workspaces syntax.';
+let MESSAGE = "Import modules from platform using workspaces syntax.";
 
 module.exports = {
   meta: {
     docs: {
       description: MESSAGE,
-      category: 'Best Practices',
+      category: "Best Practices",
       recommended: false,
     },
-    type: 'suggestion',
+    type: "suggestion",
   },
-  create: context => ({
+  create: (context) => ({
     ImportDeclaration(node) {
       const value = node.source.value;
-      if (value.startsWith('platform/')) {
-        const [_, folder] = value.split('/');
-        // these folders in platform have been converted to workspaces
-        // when all folders converted we don't have to check if a folder has been converted
-        const platformWorkspacesFolders = [
-          'forms',
-          'forms-system',
-          'landing-pages',
-          'polyfills',
-          'startup',
-          'static-data',
-          'utilities',
-          'site-wide',
-          'user',
-        ];
-        // the import is from a folder in platform that is a workspace but is not using the new syntax
-        if (platformWorkspacesFolders.includes(folder)) {
-          context.report({
-            node,
-            message: `Importing from platform via platform/${folder} is deprecated. Import from @department-of-veterans-affairs/platform-${folder} instead.`,
-          });
-        }
+      if (value.startsWith("platform/")) {
+        const [_, folder] = value.split("/");
+        context.report({
+          node,
+          message: `Importing from platform via platform/${folder} is deprecated. Import from @department-of-veterans-affairs/platform-${folder} instead or check babel.config.json for an alias to the import.`,
+        });
       }
     },
   }),
