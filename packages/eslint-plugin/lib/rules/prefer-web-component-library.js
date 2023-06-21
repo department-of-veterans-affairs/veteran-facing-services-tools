553dff7e91e12fb5606a7f6b5cdeb480d8648821
Jami Gibbs <jami.gibbs@adhocteam.us>
2023-06-21 14:44:31 -0500

    Remove table transformer

--- packages/eslint-plugin/lib/rules/prefer-web-component-library.js
+++ packages/eslint-plugin/lib/rules/prefer-web-component-library.js
@@ -247,92 +247,6 @@ const paginationTransformer = (context, node) => {
   });
 };
 
-const tableTransformer = (context, node) => {
-  const componentName = node.openingElement.name;
-  const currentSortNode = getPropNode(node, 'currentSort');
-  const dataNode = getPropNode(node, 'data');
-  const fieldsNode = getPropNode(node, 'fields');
-  const dataValue = dataNode?.value.expression || dataNode?.value;
-  const fieldsValue = fieldsNode?.value.expression || fieldsNode?.value;
-  const sourceCode = context.getSourceCode();
-  const importNode = context
-    .getAncestors()[0]
-    .body.find(
-      node =>
-        node.type === 'ImportDeclaration' &&
-        node.source.value.includes(
-          `@department-of-veterans-affairs/component-library/${componentName.name}`,
-        ),
-    );
-
-  context.report({
-    node,
-    message: MESSAGE,
-    data: {
-      reactComponent: componentName.name,
-      webComponent: 'va-table',
-    },
-    suggest: [
-      {
-        desc: 'Migrate component',
-        fix: fixer => {
-          return [
-            // Import utility function that can easily handle the existing props
-            // for the React version of `<Table>`
-            fixer.replaceText(
-              importNode.specifiers[0].local,
-              `{ generateTableChildren }`,
-            ),
-
-            // Remove Table name from import path
-            fixer.replaceText(
-              importNode.source,
-              `'@department-of-veterans-affairs/component-library'`,
-            ),
-
-            // Rename component tag
-            fixer.replaceText(componentName, 'va-table'),
-
-            // Remove the data prop
-            fixer.remove(dataNode),
-
-            // Remove the fields prop
-            fixer.remove(fieldsNode),
-
-            // Remove the currentSort prop
-            currentSortNode && fixer.remove(currentSortNode),
-
-            // Add `sort-column` prop
-            currentSortNode &&
-              fixer.insertTextBefore(currentSortNode, 'sort-column="0"'),
-
-            // Add `descending` prop
-            currentSortNode &&
-              fixer.insertTextBefore(currentSortNode, ' descending'),
-
-            // Add a closing tag along with calling the helper
-            // function to generate children
-            fixer.insertTextAfter(
-              node.openingElement,
-              `{generateTableChildren(${sourceCode.getText(
-                dataValue,
-              )}, ${sourceCode.getText(fieldsValue)})}</va-table>`,
-            ),
-            // Remove self-closing tag slash
-            fixer.removeRange(
-              [
-                node.openingElement.range[1] - 2,
-                node.openingElement.range[1] - 1,
-              ],
-              '',
-            ),
-          ].filter(i => !!i);
-        },
-      },
-    ],
-  });
-};
-
 const ombInfoTransformer = (context, node) => {
   const openingTagNode = node.openingElement.name;
   const closingTagNode = node.closingElement?.name;
@@ -459,9 +373,6 @@ module.exports = {
             break;
           case 'Pagination':
             paginationTransformer(context, node);
-            break;
-          case 'Table':
-            tableTransformer(context, node);
             break;
           case 'Telephone':
             telephoneTransformer(context, node);
