fe6a3122a862e1dace712fd0c270079b46aa49e3
Adrian Rollett <101649+acrollet@users.noreply.github.com>
2025-06-17 22:53:41 +0000

    Update jest packages

--- packages/formation/jest.config.js
+++ packages/formation/jest.config.js
@@ -5,4 +5,5 @@ module.exports = {
   testEnvironment: 'jsdom',
   rootDir: '../..',
   testMatch: [`<rootDir>/packages/formation/__tests__/**/*.test.js`],
+  setupFiles: ['<rootDir>/packages/formation/__tests__/matchMedia.mock'],
 };
