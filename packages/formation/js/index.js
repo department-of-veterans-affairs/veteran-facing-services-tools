138f4fd6744b1edbf40d312a5d1d1041d3bbf24b
KerryMin <kerrym175@gmail.com>
2025-08-07 17:49:02 -0400

    Fixed the failing Formation tests

--- packages/formation/js/index.js
+++ packages/formation/js/index.js
@@ -2,9 +2,15 @@ import loadAccordionHandler from './accordion';
 import createAdditionalInfoWidget from './additional-info';
 import addSidenavListeners from './sidenav';
 import domready from 'domready';
-import elementClosest from 'element-closest';
+import rawElementClosest from 'element-closest';
 
-elementClosest(window);
+// element-closest is a CommonJS module that exports a function directly.
+// When imported via Babel/ESM, the default export may be undefined.
+const elementClosest = typeof rawElementClosest === 'function' ? rawElementClosest : rawElementClosest.default;
+
+if (typeof elementClosest === 'function') {
+  elementClosest(window);
+}
 
 if (
   document.readyState === 'complete' &&
