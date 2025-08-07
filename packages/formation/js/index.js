f90f994047a2b3b014685ed08f52f1469b6190b3
KerryMin <kerrym175@gmail.com>
2025-08-07 19:21:40 -0400

    cleanup

--- packages/formation/js/index.js
+++ packages/formation/js/index.js
@@ -4,8 +4,7 @@ import addSidenavListeners from './sidenav';
 import domready from 'domready';
 import rawElementClosest from 'element-closest';
 
-// element-closest is a CommonJS module that exports a function directly.
-// When imported via Babel/ESM, the default export may be undefined.
+
 const elementClosest = typeof rawElementClosest === 'function' ? rawElementClosest : rawElementClosest.default;
 
 if (typeof elementClosest === 'function') {
